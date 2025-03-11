fd6fe3eff48652bb11848569d48941347330d558
Henrique <henrique.sagara@hotmail.com>
2025-03-11 10:16:48 -0400

    warnings update

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./ScrollUp.css";
 
 const ScrollUp = () => {
-  window.addEventListener("scroll", function () {
-    const scrollUp = document.querySelector(".scrollup");
-    this.scrollY >= 560
-      ? scrollUp.classList.add("show-scroll")
-      : scrollUp.classList.remove("show-scroll");
-  });
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollUp = document.querySelector(".scrollup");
+      if (scrollUp) {
+        window.scrollY >= 560
+          ? scrollUp.classList.add("show-scroll")
+          : scrollUp.classList.remove("show-scroll");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
-    <a href="#" className="scrollup">
+    <button
+      className="scrollup"
+      onClick={scrollToTop}
+      aria-label="Scroll to top"
+    >
       <i className="uil uil-arrow-up scrollup__icon"></i>
-    </a>
+    </button>
   );
 };
 
